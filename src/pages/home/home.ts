f5f80ad34d1fb6bc1c62f45a571b056d5154a19d
liutao <904620272@qq.com>
2017-06-12 22:47:36 +0800

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {LoginPage} from "../login/login";
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
 
   }
